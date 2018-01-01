6551c08629d1527c6bdf30f91ff00f2d19d44b52
Vasanth <vasanthkumar.km@gmail.com>
2018-01-01 17:37:11 -0600

    Added a comment

--- src/main/java/com/walmart/ticketservice/util/EventBookingAlgorithm.java
+++ src/main/java/com/walmart/ticketservice/util/EventBookingAlgorithm.java
@@ -3,6 +3,11 @@ package com.walmart.ticketservice.util;
 import com.walmart.ticketservice.modal.Availability;
 import com.walmart.ticketservice.modal.SeatStatus;
 
+/**
+ * Uses greedy to find requested number of consecutive seats in level
+ * and If not found, Uses backtracking to find best way to accommodate most available
+ * consecutive seats.
+ */
 public interface EventBookingAlgorithm {
     Availability[] computeBestAvailableSeats(SeatStatus[][] reservationStatus, int numberOfSeatsRequired);
 }
